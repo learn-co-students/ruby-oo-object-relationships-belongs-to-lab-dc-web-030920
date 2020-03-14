6f9854a7069d78d62480cd56dfd255381c5d6449
kaidornel <kai_dornel@hotmail.com>
2020-03-13 22:40:11 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,17 @@
 class Song
+    attr_accessor :name, :artist
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
-    attr_accessor :title, :artist
+    def self.all
+        @@all
+    end
 
+    def artist_name
+        artist.name if artist
+    end
 end
+
